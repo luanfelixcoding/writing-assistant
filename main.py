dd58a34f12b9de139ed80cae9327f94dd5cea32c
luan <luanfpcoding@gmail.com>
2025-04-03 12:21:33 -0300

    Ensured F9 triggers fix_current_line function and F10 triggers fix_selection function. Added clipboard handling with pyperclip for modifying selected text and returning the reverse of the string selected to prove the funcionality.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from pynput import keyboard
 from pynput.keyboard import Key, Controller
+import pyperclip
+import time
+
+controller = Controller()
+
+
+def fix_text(text: str) -> str:
+    return text[::-1]
 
 
 def fix_current_line():
@@ -7,7 +15,24 @@ def fix_current_line():
 
 
 def fix_selection():
-    ...
+    # 1. copy from clipboard
+    with controller.pressed(Key.ctrl):
+        controller.tap('c')
+
+    # 2. get the text from clipboard
+    time.sleep(0.1)
+    text = pyperclip.paste()
+
+    # 3. fix text
+    fixed_text = fix_text(text)
+
+    # 4. copy back to clipboard
+    pyperclip.copy(fixed_text)
+    time.sleep(0.1)
+
+    # 5. insert/show text
+    with controller.pressed(Key.ctrl):
+        controller.tap('v')
 
 
 def on_f9():
