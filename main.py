d93e37da1f4562a0f10948b6aa761acc25c649c9
luan <luanfpcoding@gmail.com>
2025-04-06 00:34:19 -0300

    Ollama setup done. Ollama mistral:7b-instructu-v0.2-q4_K_S model installed locally. Including one more library 'httpx' to treat ollama request manipulations and settings. fix_text function implemented for executing the fixing of the text using Ollama.

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 from pynput import keyboard
 from pynput.keyboard import Key, Controller
+from string import Template
 import pyperclip
 import time
+import httpx
 
 controller = Controller()
 
 
+OLLAMA_ENDPOINT = "http://localhost:11434/api/generate"
+OLLAMA_CONFIG = {"model": "mistral:7b-instruct-v0.2-q4_K_S",
+                 "keep_alive": "5m",
+                 "stream": False,
+                 }
+
+PROMPT_TEMPLATE = Template(
+    """Fix all typos and casing and punctuation in this text, but preserve all new line characters:
+    
+    $text
+    
+    Return only the corrected text, don't include a preamble.
+    """
+)
+
+
 def fix_text(text: str) -> str:
-    return text[::-1]
+    prompt = PROMPT_TEMPLATE.substitute(text=text)
+    response = httpx.post(OLLAMA_ENDPOINT,
+                          json={"prompt": prompt, **OLLAMA_CONFIG},
+                          headers={"Content-Type": "application/json"},
+                          timeout=20)
+    if response.status_code != 200:
+        return None
+    return response.json()["response"].strip()
 
 
 def fix_current_line():
     # Ctrl + Shift + Left
-    controller.press(Key.ctrl)
     controller.press(Key.shift)
-    controller.press(Key.left)
+    controller.press(Key.home)
 
-    controller.release(Key.ctrl)
     controller.release(Key.shift)
-    controller.release(Key.left)
+    controller.release(Key.home)
 
     fix_selection()
 
